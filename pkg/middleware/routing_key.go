9b175236c522a1c85a6034626fbf6544e3a9912c
Juan Ignacio Díaz <diazjignacio@gmail.com>
2023-10-07 15:42:47 -0300

    simplify routing key generation

--- pkg/middleware/routing_key.go
+++ pkg/middleware/routing_key.go
@@ -1,32 +1,28 @@
 package middleware
 
 import (
-	"hash"
 	"hash/fnv"
 	"strconv"
-	"strings"
 )
 
-type KeyGenerator struct {
-	h   hash.Hash32
-	mod int
-}
+type KeyGenerator int
 
 func NewKeyGenerator(mod int) KeyGenerator {
-	return KeyGenerator{
-		h:   fnv.New32(),
-		mod: mod,
-	}
+	return KeyGenerator(mod)
 }
 
 func ShardKey(id string) string {
 	return id
 }
 
-func (kg KeyGenerator) KeyFrom(airports ...string) string {
-	kg.h.Write([]byte(strings.Join(airports, ".")))
-	v := kg.h.Sum32()
-	kg.h.Reset()
+func (kg KeyGenerator) KeyFrom(origin, destiny string) string {
+	h := fnv.New32()
 
-	return strconv.Itoa(v % kg.mod + 1)
+	h.Write([]byte(origin))
+	h.Write([]byte("."))
+	h.Write([]byte(destiny))
+
+	v := h.Sum32()%uint32(kg) + 1
+
+	return strconv.Itoa(v)
 }

b44c85fe458cc5c99279638908fa13fce58f7b6a
Juan Ignacio Díaz <diazjignacio@gmail.com>
2023-11-16 09:04:52 -0300

    split creation and linking of temp file

--- pkg/state/store_linux.go
+++ pkg/state/store_linux.go
@@ -2,26 +2,38 @@ package state
 
 import (
 	"os"
-	"path/filepath"
 	"strconv"
 
 	"golang.org/x/sys/unix"
 )
 
-func Store(file string, p []byte) error {
-	fd, err := unix.Open(filepath.Dir(file), unix.O_TMPFILE|unix.O_WRONLY, 0644)
+func OpenTmp() (*os.File, error) {
+	fd, err := unix.Open(os.TempDir(), unix.O_TMPFILE|unix.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	return os.NewFile(uintptr(fd), "/proc/self/fd/"+strconv.Itoa(fd)), nil
+}
+
+func LinkTmp(f *os.File, name string) error {
+	if err := f.Sync(); err != nil {
+		return err
+	}
+
+	return unix.Linkat(unix.AT_FDCWD, f.Name(), unix.AT_FDCWD, name, unix.AT_SYMLINK_FOLLOW)
+}
+
+func Store(filename string, p []byte) error {
+	f, err := OpenTmp()
 	if err != nil {
 		return err
 	}
-	f := os.NewFile(uintptr(fd), "/proc/self/fd/"+strconv.Itoa(fd))
 	defer f.Close()
 
 	if _, err = f.Write(p); err != nil {
 		return err
 	}
-	if err = f.Sync(); err != nil {
-		return err
-	}
 
-	return unix.Linkat(unix.AT_FDCWD, f.Name(), unix.AT_FDCWD, file, unix.AT_SYMLINK_FOLLOW)
+	return LinkTmp(f, filename)
 }

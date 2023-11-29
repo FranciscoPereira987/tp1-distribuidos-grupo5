371ad1cdcdc50e7a34b2ce4eed59911ed4f29461
Juan Ignacio Díaz <diazjignacio@gmail.com>
2023-11-28 21:09:23 -0300

    update test script

--- test/test.bash
+++ test/test.bash
@@ -1,10 +1,36 @@
 #!/usr/bin/env bash
 
-set -e
+mkdir -p test/diff test/expected
 
-mkdir -p test/diff
+dataset=test/expected/$(stat -c'%s' client/data/test.csv)
 
-diff <(sort test/expected/first.csv) <(sort client/results/first.csv) >test/diff/first.diff
-diff <(sort test/expected/second.csv) <(sort client/results/second.csv) >test/diff/second.diff
-diff <(sort test/expected/third.csv) <(sort client/results/third.csv) >test/diff/third.diff
-diff <(sort test/expected/fourth.csv) <(sort client/results/fourth.csv) >test/diff/fourth.diff
+yesno () {
+  printf '%s ' "$*" '[Y/n]'
+  read -r ans
+  case "$ans" in
+    y|Y|'')
+      ;;
+    *)
+      return 1
+      ;;
+  esac
+}
+
+if ! [ -d "$dataset" ]
+then
+    if yesno 'New dataset. Store test data to compare against later runs?'
+    then
+        mkdir "$dataset"
+        mv client/results/* "$dataset"
+    fi
+    exit
+fi
+
+test_query() {
+    diff <(sort $dataset/$1.csv) <(sort client/results/$1.csv) >test/diff/$1.diff || echo $1 query failed
+}
+
+test_query first
+test_query second
+test_query third
+test_query fourth

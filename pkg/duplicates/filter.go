bd300e30be8608c9844bb7827aef9ba9fc30850f
Francisco Pereira <65984151+FranciscoPereira987@users.noreply.github.com>
2023-11-30 15:32:38 +0000

    Add support for more than one duplicate message to DuplicateFilter

--- pkg/duplicates/filter.go
+++ pkg/duplicates/filter.go
@@ -2,27 +2,28 @@ package duplicates
 
 import (
 	"bytes"
-	"context"
 
-	"github.com/franciscopereira987/tp1-distribuidos/pkg/middleware"
 	"github.com/franciscopereira987/tp1-distribuidos/pkg/state"
 	"github.com/franciscopereira987/tp1-distribuidos/pkg/typing"
 )
 
-type DuplicateFilterConfig struct {
-	Ctx        context.Context
-	Mid        *middleware.Middleware
-	StreamName string
-	StateFile  string
-}
+const (
+	MaxMessages = 1000
+)
 
 type DuplicateFilter struct {
+	lastMessages map[string]bool
+	queueLast    []string
+
 	LastMessage string
 }
 
 func NewDuplicateFilter(lastMessage string) *DuplicateFilter {
+
 	return &DuplicateFilter{
-		LastMessage: string(lastMessage),
+		lastMessages: make(map[string]bool),
+		queueLast:    make([]string, 0),
+		LastMessage:  string(lastMessage),
 	}
 }
 
@@ -31,15 +32,28 @@ func (df DuplicateFilter) AddToState(stateMan *state.StateManager) {
 }
 
 func (df *DuplicateFilter) RecoverFromState(stateMan *state.StateManager) {
-	if value := stateMan.Get("last-received"); value != nil {
-		df.LastMessage = value.(string)
+	if value := stateMan.GetString("last-received"); value != "" {
+		df.LastMessage = value
+	}
+}
+
+func (df *DuplicateFilter) manageSizes() {
+	if len(df.queueLast) >= MaxMessages {
+		delete(df.lastMessages, df.queueLast[0])
+		df.queueLast = df.queueLast[1:]
 	}
 }
 
 func (df *DuplicateFilter) ChangeLast(newLastMessage []byte) (*bytes.Reader, error) {
 	r := bytes.NewReader(newLastMessage)
 	h, err := typing.HeaderUnmarshall(r)
-	df.LastMessage = h.ID
+
+	if err == nil {
+		df.lastMessages[h.ID] = true
+		df.queueLast = append(df.queueLast, h.ID)
+		df.LastMessage = h.ID
+		df.manageSizes()
+	}
 
 	return r, err
 }

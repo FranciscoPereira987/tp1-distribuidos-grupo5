f237bfa54d1769dd2b504b3a04cbfda378d734a2
Juan Ignacio Díaz <diazjignacio@gmail.com>
2023-12-06 19:51:51 -0300

    use empty line as EOF

--- pkg/protocol/protocol.go
+++ pkg/protocol/protocol.go
@@ -9,11 +9,11 @@ import (
 
 var ErrNotQueryResult = errors.New("not a query result")
 
-var ResultEOF = []string{"0"}
+var ResultEOF []string
 
 func SplitRecord(record []byte) (int, []byte, error) {
 	if len(record) == 0 {
-		return 0, nil, io.ErrShortBuffer
+		return 0, nil, io.EOF
 	}
 	head, tail, _ := bytes.Cut(record, []byte(","))
 	if len(head) != 1 {
@@ -23,9 +23,6 @@ func SplitRecord(record []byte) (int, []byte, error) {
 	tag := int(head[0] - '0')
 	if tag > 0 && tag < 5 {
 		return tag, tail, nil
-	}
-	if tag == 0 {
-		return 0, nil, io.EOF
 	}
 
 	return 0, nil, fmt.Errorf("%w: tag=%d", ErrNotQueryResult, tag)
